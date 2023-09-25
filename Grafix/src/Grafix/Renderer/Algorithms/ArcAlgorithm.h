2861ecbf5c6f1e8a6b0498404beabf597d63b61d
ycshao21 <mematthew21@gmail.com>
2023-09-25 22:00:38 +0800

    Test Conflict

--- Grafix/src/Grafix/Renderer/Algorithms/ArcAlgorithm.h
+++ Grafix/src/Grafix/Renderer/Algorithms/ArcAlgorithm.h
@@ -12,4 +12,11 @@ namespace Grafix
         static void Midpoint(const glm::vec2& center, float radius, const glm::vec4& color);
         static void SetCirclePixels(int centerX, int centerY, int x, int y, const glm::vec4& color);
     };
+
+    class Test
+    {
+    public:
+        Test();
+        ~Test();
+    };
 }
