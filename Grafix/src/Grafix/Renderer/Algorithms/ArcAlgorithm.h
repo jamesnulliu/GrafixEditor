f100826113fbd780a60d8731a8318bbdd43fc863
wj <783453100@qq.com>
2023-09-25 21:59:09 +0800

    Arc

--- Grafix/src/Grafix/Renderer/Algorithms/ArcAlgorithm.h
+++ Grafix/src/Grafix/Renderer/Algorithms/ArcAlgorithm.h
@@ -7,7 +7,8 @@ namespace Grafix
     class ArcAlgorithm : public GraphicsAlgorithm
     {
     public:
-        static void Draw(const glm::vec2& center, float radius, const glm::vec4& color);
+        static void Draw(const glm::vec2& center, float radius, float startAngle, float endAngle, const glm::vec4& color);
+
     private:
         static void Midpoint(const glm::vec2& center, float radius, const glm::vec4& color);
         static void SetCirclePixels(int centerX, int centerY, int x, int y, const glm::vec4& color);
